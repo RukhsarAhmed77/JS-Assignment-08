c60b6ef4314954307deffb7a208fe381217bec0a
Rukhsar Ahmed <rukhsarnaich0@gmail.com>
2024-10-03 20:48:22 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+//JS Assignment 08
+
+//Task 01
+
+// var arr = [12, 1, 10, 3, 4, 2, 8, 5, 7, 9, 6, 11];
+// var min;
+
+// for (var i = 0; i < arr.length; i++) {
+//     min = i;
+//     for (var j = i + 1; j < arr.length; j++) {
+//       if (arr[j] < arr[min]) {
+//         min = j;
+//       }
+//     }
+//     var temp = arr[i];
+//     arr[i] = arr[min];
+//     arr[min] = temp;
+// }
+
+// console.log("Sorted array in ascending order:", arr);
+
+
+// Task 02
+
+// var arr = [12, 1, 10, 3, 4, 2, 8, 5, 7, 9, 6, 11];
+// var max;
+
+// for (var i = 0; i < arr.length; i++) {
+//     max = i;
+//     for (var j = i + 1; j < arr.length; j++) {
+//       if (arr[j] > arr[max]) {
+//         max = j;
+//       }
+// }
+// var temp = arr[i];
+// arr[i] = arr[max];
+// arr[max] = temp;
+// }
+
+// console.log("Sorted array in descending order:", arr);
